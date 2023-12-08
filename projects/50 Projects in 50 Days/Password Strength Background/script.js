5ccc96397fc6beeef76ebe0dc0b646502f873567
Joshua Scott <joshua.ds.s019@gmail.com>
2023-12-08 11:48:02 -0800

    created 3D boxes background proj

--- projects/50 Projects in 50 Days/Password Strength Background/script.js
+++ projects/50 Projects in 50 Days/Password Strength Background/script.js
@@ -3,6 +3,5 @@ const password = document.getElementById('password')
 
 password.addEventListener('input', () => {
     const passwordLength = password.value.length
-
     background.style.filter = `blur(${20 - (passwordLength * 2)}px)`
 })
