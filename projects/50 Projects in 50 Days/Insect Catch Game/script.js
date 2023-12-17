e997aa247fbdfd9dde8a5fc05bd4d7720816f63e
Joshua Scott <joshua.ds.s019@gmail.com>
2023-12-17 09:08:49 -0800

    added random movement and rotation to insects in bug catch game as well as temp art assets

--- projects/50 Projects in 50 Days/Insect Catch Game/script.js
+++ projects/50 Projects in 50 Days/Insect Catch Game/script.js
@@ -25,27 +25,43 @@ chooseInsectButtons.forEach(btn => {
 
 function startGame() {
     setInterval(increaseTime, 1000)
-    // checkForInsects()
+    setInterval(updateBugs, 100)
 }
 
-// function checkForInsects() {
-//     const insects = document.querySelectorAll('.insect')
-//     if (insects.length > 0) {
-//         insects.forEach(bug => {
-//             setInterval(() => {
-//                 const rect = bug.getBoundingClientRect()
-//                 const currentX = Math.round(rect.left)
-//                 const currentY = Math.round(rect.top)
-//                 // console.log(`X:${currentX}/${window.innerWidth}, Y:${currentY}/${window.innerHeight}`);
-//                 const { x, y } = getRandomLocation()
-//                 bug.style.top = `${y}px`
-//                 bug.style.left = `${x}px`
-//                 bug.style.transition = `all 1s ease-in-out`
-//                 bug.style.transform = `rotate(${calcRotation(currentX, currentY, x, y)}deg)`
-//             }, 1000)
-//         })
-//     }
-// }
+function updateBugs() {
+    const bugs = document.querySelectorAll('.insect')
+    if (bugs.length > 0) {
+        bugs.forEach(bug => {
+            if (bug.classList.contains('active') === false) {
+                const getRandom = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+                bug.classList.add('active')
+                bug.firstChild.classList.add('wiggleBug')
+                const rect = bug.getBoundingClientRect()
+                const currX = Math.floor(rect.left)
+                const currY = Math.floor(rect.top)
+                const newX = getRandom(200, window.innerWidth - 200)
+                const newY = getRandom(200, window.innerHeight - 200)
+                bug.style.left = `${newX}px`
+                bug.style.top = `${newY}px`
+                const deg = angle(newX, newY, currX, currY)
+                bug.style.transform = `rotate(${deg}deg)`
+                // console.log(`X:${currX}/${newX}, Y:${currY}/${newY}`);
+                setTimeout(() => bug.firstChild.classList.remove('wiggleBug'), 2000)
+                setTimeout(() => bug.classList.remove('active'), 3000)
+            }
+        })
+    }
+}
+
+function angle(cx, cy, ex, ey) {
+    var dy = ey - cy;
+    var dx = ex - cx;
+    var theta = Math.atan2(dy, dx); // range (-PI, PI]
+    theta *= 180 / Math.PI; // rads to degs, range (-180, 180]
+    //if (theta < 0) theta = 360 + theta; // range [0, 360)
+    return theta;
+}
+
 
 
 
@@ -72,10 +88,10 @@ function createInsect() {
     const { x, y } = getRandomLocation()
     insect.style.top = `${y}px`
     insect.style.left = `${x}px`
-    insect.innerHTML = `<img src="${selectedInsect.src}" alt="${selectedInsect.alt}" style="transform: rotate(${Math.random() * 360}deg)" draggable="false"/>`
+    // insect.innerHTML = `<img src="${selectedInsect.src}" alt="${selectedInsect.alt}" style="transform: rotate(${Math.random() * 360}deg)" draggable="false"/>`
+    insect.innerHTML = `<img src="${selectedInsect.src}" alt="${selectedInsect.alt}" draggable="false"/>`
     insect.addEventListener('click', catchInsect)
     gameContainer.appendChild(insect)
-    // checkForInsects()
 }
 
 function getRandomLocation() {
@@ -88,8 +104,9 @@ function getRandomLocation() {
 
 function catchInsect() {
     increaseScore()
-    this.classList.add('caught')
-    setTimeout(() => this.remove(), 2000)
+    this.remove()
+    // this.classList.add('caught')
+    // setTimeout(() => this.remove(), 2000)
     addInsects()
 }
 
